274af1eebfc5a4498691d3bff6dedb8c881dc82e
fartun <fartunwario34@gmail.com>
2023-04-07 10:09:10 +0300

    completed with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+const taskForm = document.querySelector('#create-task-form');
+const taskList = document.querySelector('#tasks');
+
+taskForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const newTaskInput = document.querySelector('#new-task-description');
+  const newTask = document.createElement('li');
+  newTask.innerText = newTaskInput.value;
+  taskList.appendChild(newTask);
+  newTaskInput.value = '';
+});
+
 });
